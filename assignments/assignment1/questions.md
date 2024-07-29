6194e66ed8a3bcb988a252dca944c66f791340e9
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2024-07-29 20:38:30 +0530

    arithmatic, relational, assignment, pre-post inc/dec operator, assignment 1

---
+++ assignments/assignment1/questions.md
@@ -0,0 +1,37 @@
+# Assignment 1
+
+1. Write a java program to add and display 4 and 5.
+2. Write a java program to evaluate `c += ++c + c++`, where initial value of c
+   is 5.
+3. Write a java program to find out and display when `&` and `|` operator is
+   applied between integers. Find out why.
+
+```java
+    int a = 3;
+    int b = 4;
+    System.out.println(a & b);
+    System.out.println(a | b);
+```
+
+4. Which data types are best to store following values.
+
+     - 125
+     - 50
+     - 1352
+     - 9439485166
+     - 9439.485166
+     - a persons age
+     - 1
+     - either 0 or 1
+
+5. Try to find out what is the output of the program and why?
+
+```java
+
+    short a = 86;
+    short b = 75;
+    short c = a + b;
+
+    System.out.println(c);
+
+```
---
+++ operations/IntOperations.java
@@ -0,0 +1,98 @@
+public class IntOperations {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c;
+
+        // Arithmetic Operations
+        // int = int <arithmetic operator> int
+        // int = int / 0 java.lang.ArithmeticException: / by zero
+        // int = int % 0 java.lang.ArithmeticException: / by zero
+        System.out.println(a + b); // 5
+        System.out.println(a - b); // -1
+        System.out.println(a * b); // 6
+        System.out.println(a / b); // 0
+        System.out.println(a % b); // 2
+
+        c = a + b;
+        System.out.println(c);
+        c = a - b;
+        System.out.println(c);
+        c = a * b;
+        System.out.println(c);
+        c = a / b;
+        System.out.println(c);
+        c = a % b;
+        System.out.println(c);
+
+        // Relational Operator
+        // boolean = int <relational operator> int
+        System.out.println(a < b);// true
+        System.out.println(a > b);// false
+        System.out.println(a <= b);// true
+        System.out.println(a >= b);// false
+        System.out.println(a == b);// false
+        System.out.println(a != b);// true
+
+        // Assignment operator
+        // Rule 1: When assignment operator is used the right side value is
+        // evaluated first and assigned to left variable
+        // Rule 2: If we try to print and assignment statement then always the left side
+        // variable will be displayed.
+        System.out.println(a = b);// a = 3,b = 3 -> 3
+        System.out.println(a += b);// a = 6,b = 3 -> 6 // a = a+b
+        System.out.println(a -= b);// a = 3,b = 3 -> 3 // a = a-b
+        System.out.println(a *= b);// a = 9 ,b = 3 -> 9 // a = a*b
+        System.out.println(a /= b);// a = 3, b = 3 -> 3 // a = a/b
+        System.out.println(a %= b);// a = 0, b = 3 -> 0 // a = a%b
+
+        System.out.println(b = a);// a = 0, b = 0
+        System.out.println(b += a);// a = 0, b = 0
+        System.out.println(b -= a);// a = 0, b = 0
+        System.out.println(b *= a);// a = 0, b= 0
+        // System.out.println(b /= a);// a = 0, b= 0 java.lang.ArithmeticException: / by
+        // zero because / 0 is not possible
+        // System.out.println(b %= a);// a = 0, b = 0
+        System.out.println("__________________________________________");
+        a = 2;
+        b = 3;
+        System.out.println(c = a + b);// 5
+        System.out.println(c += a + b);// 10
+        System.out.println(c -= a + b);// 5
+        System.out.println(c *= a + b);// 25
+        System.out.println(c /= a + b);// 5
+        System.out.println(c %= a + b);// 0
+
+        System.out.println("________________________________________");
+        System.out.println(c += a += b);// 5
+        System.out.println(c += a -= b);// 7
+        System.out.println(c -= a += b);// 2
+
+        System.out.println(c += c += c += c);// 8
+
+        // Increment Decrement operator
+        // if pre increment or decrement then the value will be evaluated and updated
+        // first then fetched
+        // if post increment or decrement then the value will be fetched first then
+        // evaluated and updated
+        // c = 10
+        System.out.println(c++);// 8
+        System.out.println(++c);// 10
+        System.out.println(c--);// 10
+        System.out.println(--c);// 8
+        System.out.println(c++ + ++c);// 18
+        System.out.println(c);// 10
+        System.out.println(++c + c++);// 22
+        System.out.println(c);// 12
+
+        System.out.println(c-- - --c);// 2
+        System.out.println(c);// 10
+        System.out.println(--c - c--);// 9 - 9
+        System.out.println(c);// 8
+        System.out.println(c++ + c++);// 16 8 + 9
+        System.out.println(c);// 10
+        System.out.println(c-- - c--);// 1
+        System.out.println(c);// 8
+
+    }
+}
