5e3f012d8d473cc6464803a3af469cc1a8fddd83
Mousumi Mohanty <mousumimohanty231@gmail.com>
2024-07-30 17:36:05 +0530

    conditional

---
+++ conditionals/ElseIf.java
@@ -0,0 +1,15 @@
+public class ElseIf {
+   public static void main(String[] args) {
+      int a=8;
+      int b=4;
+      int c=9;
+
+      if (a>b && a>c ) 
+          System.out.println(a);
+       else if (b>a && b>c)
+           System.out.println(b); 
+        else 
+            System.out.println(c);          
+      
+   } 
+}
