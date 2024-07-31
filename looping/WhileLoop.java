2983ac87c7b3354aeecd7a666ac4628b9d47a69a
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2024-07-31 19:41:11 +0530

    while practice

---
+++ looping/WhileLoop.java
@@ -0,0 +1,48 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        /*
+         * Syntax
+         * while(condition){
+         * code to be executed
+         * }
+         */
+        // int i = 0;
+        // while (i < 10) {
+        // System.out.println("Before if : " + i);
+        // if (i % 2 == 0) {
+        // i++;
+        // continue;
+        // }
+        // System.out.println(i);
+        // i++;
+        // }
+        // // System.out.println(i);
+
+        // Q1. Print numbers from 1 to n
+        // int n = 1;
+        // while (n <= 10) {
+        // System.out.println(n);
+        // n++;
+        // }
+        // System.out.println(n);// 11
+
+        // Q2.Write a program that calculates the sum of the digits of a given number.
+        int n = 12356;
+
+        // initialize a variable named sum with 0
+        // repetitively get the last digit from the number and add it to sum
+        // do modulo with 10 the number to get last digit
+        // update the number by dividing with 10
+        // int sum = 0;
+        // while (n != 0) {
+        // int lastDigit = n % 10;
+        // // System.out.println("Value of n: " + n + "\tValue of lastDigit: " +
+        // // lastDigit);
+        // sum += lastDigit;
+        // n /= 10;
+        // }
+        // System.out.println(sum);
+
+        // Q3. Write a program that reverses the digits of a given number.
+    }
+}
