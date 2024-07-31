16b97408d8f47cb3a9e958895dfd940f01883ffe
Mousumi Mohanty <mousumimohanty231@gmail.com>
2024-07-31 19:08:23 +0530

    4

--- assignments/assignment2/Qn_4.java
+++ assignments/assignment2/Qn_4.java
@@ -1,22 +1,19 @@
 public class Qn_4 {
-    public static void main(String[] args){
-        int day = 4;
-        
+
+    public static void main(String[] args) {
+        int day = 7;
+
         switch (day) {
-            
-            case 0|7:
+            case 0, 6:
                 System.out.println("playing games");
                 break;
-            
-            case 1|3:
-                System.out.println("competitive Coding");
+            case 1, 3, 5:
+                System.out.println("competitive coding");
                 break;
-            
-            case 2|4:
-                System.out.println("making project");
+            case 2, 4:
+                System.out.println("making projects");
                 break;
-            default:
-                System.out.println("invalid week days");
-                break;}
-   } 
+
+        }
+    }
 }
