8b927ac001f1dfb5148003fb9dbc4447c76f052f
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2024-08-03 19:31:30 +0530

    do while loop

--- looping/DoWhileLoop.java
+++ looping/DoWhileLoop.java
@@ -8,19 +8,40 @@ public class DoWhileLoop {
 
         // Qn: Write a C program to print numbers from 1 to 10 and 10 to 1 using a
         // do-while loop
-        int i = 1;
-        System.out.print("number from 1 to 10\n");
-        do {
-            System.out.println(i);
-            i++;
-        } while (i <= 10);
-        System.out.print("\n");
-        int j = 10;
-        System.out.print("number from 10 to 1\n");
+        // int i = 1;
+        // System.out.print("number from 1 to 10\n");
+        // do {
+        // System.out.println(i);
+        // i++;
+        // } while (i <= 10);
+        // System.out.print("\n");
+        // int j = 10;
+        // System.out.print("number from 10 to 1\n");
 
-        do {
-            System.out.println(j);
-            j--;
-        } while (j >= 1);
+        // do {
+        // System.out.println(j);
+        // j--;
+        // } while (j >= 1);
+
+        /*
+         * Do while loop is an exit control loop, where as while is a entry control
+         * loop.
+         * 
+         * Entry control loop means first the condition will be checked then the code
+         * inside it will be executed, if the condition is true.
+         * 
+         * Exit control loop means first the code inside it will be executed then it
+         * will check
+         * condition. It will continue the loop if the condition is true else exit.
+         * 
+         * The codes inside a do..while loop will be executed at least once no matter
+         * what the
+         * condition is.
+         */
+        // int i = 0;
+        // do {
+        // System.out.print(i % 2 == 0 ? i + "\n" : "");
+        // i++;
+        // } while (i < 10);
     }
 }
