83eaf31219270cdc5084e10676be4a02278d5cb3
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2024-08-29 00:40:25 +0530

    looping

---
+++ looping/Looping.java
@@ -0,0 +1,46 @@
+public class Looping {
+    public static void main(String[] args) {
+        /*
+         * Why and where loop is used?
+         * Loop is used when it is required to perform repetitive task.
+         */
+        // print 1 to 5
+        // native way
+        // System.out.println(1);
+        // System.out.println(2);
+        // System.out.println(3);
+        // System.out.println(4);
+        // System.out.println(5);
+        // using loops
+        // int i = 1;
+        // while (i < 6) {
+        // System.out.println(i);
+        // i++;
+        // }
+        /*
+         * While Loop
+         * while(condition){
+         * // code to be executed
+         * }
+         */
+
+        /*
+         * Do While Loop
+         * do{
+         * code to be executed
+         * }while(condition);
+         */
+        /*
+         * for Loop
+         * for(initialization;condition;updating){
+         * code to be executed
+         * }
+         */
+        // int counter = 1;
+        // while (counter <= 5) {
+        // System.out.println(counter);
+        // counter++;
+        // }
+
+    }
+}
