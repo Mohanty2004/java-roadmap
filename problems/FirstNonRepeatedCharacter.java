fe91ca3e299afbce992657a28b3b40916e6bbf65
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2024-08-29 00:15:35 +0530

    Fix

--- problems/FirstNonRepeatedCharacter.java
+++ problems/FirstNonRepeatedCharacter.java
@@ -3,17 +3,21 @@ package problems;
 public class FirstNonRepeatedCharacter {
     public static void main(String[] args) {
         String str = System.console().readLine();
+        boolean[] visited = new boolean[str.length()];
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             int count = 0;
             for (int j = i + 1; j < str.length(); j++) {
-                if (ch == str.charAt(j))
+                if (ch == str.charAt(j)) {
+                    visited[j] = true;
                     count++;
+                }
             }
-            if (count == 0) {
+            if (count == 0 && !visited[i]) {
                 System.out.println(ch);
                 return;
             }
+            visited[i] = true;
         }
     }
 }
