c6138b5adb8df7e8c2b046afb96be1b45b830e08
Mousumi Mohanty <mousumimohanty231@gmail.com>
2024-11-22 01:45:51 +0530

    os thread

--- threading/ThreadMethods.java
+++ threading/ThreadMethods.java
@@ -1,5 +1,39 @@
 package threading;
 
+class MynewThr extends Thread {
+
+    public void run() {
+        int i = 0;
+        while (i < 56546) {
+
+            System.out.println("Thank you");
+            i++;
+        }
+    }
+}
+
+class MynewThr2 extends Thread {
+
+    public void run() {
+
+        while (true) {
+
+            System.out.println("Thank you!");
+        }
+    }
+}
+
 public class ThreadMethods {
-    
+    public static void main(String[] args) {
+        MynewThr t1 = new MynewThr();
+        MynewThr2 t2 = new MynewThr2();
+        t1.start();
+        try {
+            t1.join();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        t2.start();
+    }
 }
